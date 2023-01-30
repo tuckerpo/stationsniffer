f285696fa3ad17b125973a2a97a1b0dead875550
Tucker Polomik <t.polomik@cablelabs.com>
2023-01-29 17:59:58 -0700

    radiotap_fields: change RSSI default value
    Initialize to -INT8_MAX.
    
    Previously, default initialization would set the RSSI field to 0.
    
    Since RSSI is typically a log-scale value, where 0 represents very high
    signal strength, change to -127.
    
    Signed-off-by: Tucker Polomik <t.polomik@cablelabs.com>

--- radiotap_fields.h
+++ radiotap_fields.h
@@ -2,7 +2,7 @@
 #include <cstdint>
 
 struct radiotap_fields {
-    int8_t rssi; // dBm
+    int8_t rssi{-127}; // dBm
     uint16_t channel_number;
     uint16_t channel_frequency; // mHz
     bool bad_fcs;               // bad frame control sequence, indicates that this is bogus data.
