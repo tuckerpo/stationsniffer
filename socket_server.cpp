48f2a4241ed7d55c069ef0c3f9b0a3838b217ffe
Tucker Polomik <t.polomik@cablelabs.com>
2023-05-05 13:05:45 -0600

    socket_server.{cpp|h}: remove manual memory management of pollfd
    Just use a vector of pollfd instead.
    
    Signed-off-by: Tucker Polomik <t.polomik@cablelabs.com>

--- socket_server.cpp
+++ socket_server.cpp
@@ -23,14 +23,6 @@ bool socket_server::begin_serving(const std::string &path, bool &keep_running)
 {
     static constexpr int listen_backlog = 10;
 
-    int fd_size  = 5;
-    pollfd *pfds = (pollfd *)malloc(sizeof(pollfd *) * fd_size);
-    memset(pfds, 0, sizeof(pollfd *) * fd_size);
-    const auto bail = [&pfds]() {
-        free(pfds);
-        return false;
-    };
-
     sockaddr_un remote = {0};
     sockaddr_un local  = {0};
     m_server_fd        = socket(AF_UNIX, SOCK_STREAM, 0);
@@ -43,37 +35,37 @@ bool socket_server::begin_serving(const std::string &path, bool &keep_running)
     unlink(path.c_str());
     if (bind(m_server_fd, (sockaddr *)&local, sizeof(local)) < 0) {
         perror("bind");
-        bail();
+        return false;
     }
     if (listen(m_server_fd, listen_backlog) < 0) {
         perror("listen");
-        bail();
+        return false;
     }
-
-    pfds[0].fd     = m_server_fd;
-    pfds[0].events = POLLIN;
-    int fd_count   = 1; // m_server_fd
+    std::vector<pollfd> pollfd_vector;
+    // Add the server pollfd to the set.
+    pollfd_vector.push_back({m_server_fd, POLLIN, 0});
     while (keep_running) {
-        int poll_count = poll(pfds, fd_count, 1000);
+        int poll_count = poll(pollfd_vector.data(), (nfds_t)pollfd_vector.size(), 1000);
         if (poll_count == -1) {
             perror("poll");
         }
-        for (int i = 0; i < fd_count; i++) {
-            if (pfds[i].revents & POLLIN) {
-                if (pfds[i].fd == m_server_fd) {
+        std::vector<int> clients_to_be_removed;
+        for (auto &pfd : pollfd_vector) {
+            if (pfd.revents & POLLIN) {
+                if (pfd.fd == m_server_fd) {
                     // new connection.
                     unsigned sock_len = 0;
                     int new_conn_fd   = accept(m_server_fd, (sockaddr *)&remote, &sock_len);
                     if (new_conn_fd == -1) {
                         perror("accept");
                     } else {
-                        add_to_poll_fdset(&pfds, new_conn_fd, fd_count, fd_size);
-                        std::cout << "New connection!" << std::endl;
+                        pollfd_vector.push_back({new_conn_fd, POLLIN, 0});
+                        std::cout << "New connection! fd=" << new_conn_fd << std::endl;
                     }
                 } else {
                     char rxbuf[256];
-                    int nbytes    = recv(pfds[i].fd, rxbuf, sizeof(rxbuf), 0);
-                    int sender_fd = pfds[i].fd;
+                    int nbytes    = recv(pfd.fd, rxbuf, sizeof(rxbuf), 0);
+                    int sender_fd = pfd.fd;
                     if (nbytes <= 0) {
                         if (nbytes == 0) {
                             std::cerr << "Client hung up on fd: " << sender_fd << std::endl;
@@ -81,21 +73,32 @@ bool socket_server::begin_serving(const std::string &path, bool &keep_running)
                             // TODO check errors better (EAGAIN, EWOULDBLOCK etc);
                             perror("recv");
                         }
-                        // error, remove from poll set
-                        close(pfds[i].fd);
-                        remove_from_poll_fdset(pfds, i, fd_count);
+                        // Mark for removal.
+                        clients_to_be_removed.push_back(pfd.fd);
                     } else {
                         // good data.
                         std::vector<uint8_t> payload;
                         payload.reserve(sizeof(rxbuf));
                         std::copy(rxbuf, rxbuf + sizeof(rxbuf), std::back_inserter(payload));
-                        m_message_handler.post_data(payload, pfds[i].fd);
+                        m_message_handler.post_data(payload, pfd.fd);
                     }
                 }
             }
         }
+        // Walk dead clients and remove them from the poll set.
+        for (const auto &dead_client_fd : clients_to_be_removed) {
+            auto pollfd_entry_to_remove = std::find_if(pollfd_vector.begin(), pollfd_vector.end(),
+                                                       [&dead_client_fd](pollfd &pollfd_entry) {
+                                                           return pollfd_entry.fd == dead_client_fd;
+                                                       });
+            if (pollfd_entry_to_remove != pollfd_vector.end()) {
+                close(pollfd_entry_to_remove->fd);
+                std::cout << "Removing pollfd entry for dead client, fd="
+                          << pollfd_entry_to_remove->fd << std::endl;
+                pollfd_vector.erase(pollfd_entry_to_remove);
+            }
+        }
     }
-    free(pfds);
     return stop_serving();
 }
 
@@ -104,21 +107,4 @@ bool socket_server::stop_serving()
     if (m_server_fd != -1)
         close(m_server_fd);
     return true;
-}
-
-void socket_server::add_to_poll_fdset(pollfd *pfds[], int new_fd, int &fd_count, int &poll_fd_size)
-{
-    if (fd_count == poll_fd_size) {
-        poll_fd_size *= 2;
-        *pfds = (pollfd *)realloc(*pfds, sizeof(**pfds) * (poll_fd_size));
-    }
-    (*pfds)[fd_count].fd     = new_fd;
-    (*pfds)[fd_count].events = POLLIN;
-    fd_count++;
-}
-
-void socket_server::remove_from_poll_fdset(pollfd pfds[], int idx, int &fd_count)
-{
-    pfds[idx] = pfds[fd_count - 1];
-    fd_count--;
 }
